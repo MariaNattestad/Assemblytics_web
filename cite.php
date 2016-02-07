a93983d5a75a3521a29cdeb4c4a57ae16c42ede1
MariaNattestad <lunexa@gmail.com>
2016-02-07 17:31:08 -0500

    Added citation page

---
+++ cite.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+
+<!--    NAVIGATION BAR-->
+<?php include "header.html";?>
+<?php include "title.html";?>
+ 
+<div class="center">
+    <div class="panel panel-info">
+      <div class="panel-heading">
+    <h3 class="panel-title">How to cite Assemblytics</h3>
+      </div>
+      <div class="panel-body">
+        <p>Please cite our paper on the BioarXiv when it becomes available. Until then, cite (Nattestad et al, in preparation) </p>
+        <br>
+        <p>The code is open-source at <a href="https://github.com/MariaNattestad/Assemblytics" target="_blank">https://github.com/MariaNattestad/Assemblytics</a></p>
+      </div>
+    </div>
+</div>
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
+
+
