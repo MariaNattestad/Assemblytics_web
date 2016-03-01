eee0640869140d5436115bef984e4d413ce08421
MariaNattestad <lunexa@gmail.com>
2016-03-01 09:45:42 -0500

    Added N chart

--- cite.php
+++ cite.php
@@ -12,7 +12,7 @@
     <h3 class="panel-title">How to cite Assemblytics</h3>
       </div>
       <div class="panel-body">
-        <p>Please cite our paper on the BioArXiv (NEED LINK) </p>
+        <p>We are writing up the paper now and will be submitting a pre-print to the BioArXiv shortly. We will post the link here as soon as we have it, so please come back and cite it if you found Assemblytics useful. </p>
         <br>
         <!-- <p>The code is open-source at <a href="https://github.com/MariaNattestad/Assemblytics" target="_blank">https://github.com/MariaNattestad/Assemblytics</a></p> -->
       </div>
