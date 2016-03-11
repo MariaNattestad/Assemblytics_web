1e27944aefa059ab9ba1b4358e8dee997851776f
MariaNattestad <lunexa@gmail.com>
2016-03-11 13:51:07 -0500

    Fixed several issues with plots, added error handling for plots that didn't exist when user ran their dataset: hiding the plots and putting a small note that if they re-run it there are new plots available.

--- bin/size_distributions.R
+++ bin/size_distributions.R
@@ -4,13 +4,13 @@ library(ggplot2)
 args<-commandArgs(TRUE)
 output_prefix <- args[1]
 
-# output_prefix <- "~/Desktop/SIMULATIONS/Assemblytics_results/Human_simulated_insertions"
+# output_prefix <- "~/tmp/Escherichia_coli_MHAP_assembly"
 
 
 filename <- paste(output_prefix,".Assemblytics_structural_variants.bed",sep="")
 
 # print(filename)
-bed <- read.csv(filename, sep="\t", quote='', header=FALSE)
+bed <- read.csv(filename, sep="\t", quote='', header=TRUE)
 
 names(bed)[1:11] <- c("chrom","start","stop","name","size","strand","type","ref.dist","query.dist","contig_position","method.found")
 # head(bed)
@@ -51,12 +51,15 @@ binwidth <- 5
 
 
 png(paste(output_prefix,".Assemblytics.size_distributions.png", sep=""),1000,1000)
-ggplot(bed[bed$size>=50,],aes(x=size, fill=type)) + geom_bar(binwidth=binwidth*10) + scale_fill_brewer(palette=color_palette_name) + facet_grid(type ~ .) + labs(fill="Structural variant type",x="Variant size",y="Count",title="Structural variants > 50 bp") + theme(strip.text=element_blank(),strip.background=element_blank())
+ggplot(bed[bed$size>=50,],aes(x=size, fill=type)) + geom_bar(binwidth=binwidth*10) + scale_fill_brewer(palette=color_palette_name,drop=FALSE) + facet_grid(type ~ .,drop=FALSE) + labs(fill="Structural variant type",x="Variant size",y="Count",title="Structural variants > 50 bp") + theme(
+    strip.text=element_blank(),strip.background=element_blank(),
+    plot.title = element_text(vjust=3)
+    )
 dev.off()
 
 
 png(paste(output_prefix,".Assemblytics.size_distributions_large_structural.png", sep=""),1000,1000)
-ggplot(bed[bed$size>=500,],aes(x=size, fill=type)) + geom_bar(binwidth=binwidth*10) + scale_fill_brewer(palette=color_palette_name) + facet_grid(type ~ .) + labs(fill="Structural variant type",x="Variant size",y="Count",title="Structural variants > 500 bp") + theme(strip.text=element_blank(),strip.background=element_blank())
+ggplot(bed[bed$size>=500,],aes(x=size, fill=type)) + geom_bar(binwidth=binwidth*10) + scale_fill_brewer(palette=color_palette_name,drop=FALSE) + facet_grid(type ~ .,drop=FALSE) + labs(fill="Structural variant type",x="Variant size",y="Count",title="Structural variants > 500 bp") + theme(strip.text=element_blank(),strip.background=element_blank(),plot.title = element_text(vjust=3))
 dev.off()
 
 
@@ -64,13 +67,18 @@ dev.off()
 
 
 png(paste(output_prefix,".Assemblytics.size_distributions_zoom_structural.png", sep=""),1000,1000)
-ggplot(bed[bed$size>=50,],aes(x=size, fill=type)) + geom_bar(binwidth=binwidth) + xlim(0,500) + scale_fill_brewer(palette=color_palette_name) + facet_grid(type ~ .) + labs(fill="Structural variant type",x="Variant size",y="Count",title="Structural variants > 50 bp") + theme(strip.text=element_blank(),strip.background=element_blank())
+ggplot(bed[bed$size>=50,],aes(x=size, fill=type)) + geom_bar(binwidth=binwidth) + xlim(0,500) + scale_fill_brewer(palette=color_palette_name,drop=FALSE) + facet_grid(type ~ .,drop=FALSE) + labs(fill="Structural variant type",x="Variant size",y="Count",title="Structural variants > 50 bp") + theme(strip.text=element_blank(),strip.background=element_blank(),plot.title = element_text(vjust=3))
 dev.off()
 
 
 
 png(paste(output_prefix,".Assemblytics.size_distributions_zoom.png", sep=""),1000,1000)
-ggplot(bed,aes(x=size, fill=type)) + geom_bar(binwidth=binwidth) + scale_fill_brewer(palette=color_palette_name) + xlim(0,500) + facet_grid(type ~ .) + labs(fill="Variant type",x="Variant size",y="Count",title="All variants > 10 bp") + theme(strip.text=element_blank(),strip.background=element_blank())
+ggplot(bed,aes(x=size, fill=type)) + geom_bar(binwidth=binwidth) + scale_fill_brewer(palette=color_palette_name,drop=FALSE) + xlim(0,500) + facet_grid(type ~ .,drop=FALSE) + labs(fill="Variant type",x="Variant size",y="Count",title="All variants > 5 bp") + theme(strip.text=element_blank(),strip.background=element_blank(),plot.title = element_text(vjust=3))
+dev.off()
+
+
+png(paste(output_prefix,".Assemblytics.size_distributions_all_variants_full_view.png", sep=""),1000,1000)
+ggplot(bed,aes(x=size, fill=type)) + geom_bar(binwidth=binwidth) + scale_fill_brewer(palette=color_palette_name,drop=FALSE) + facet_grid(type ~ .,drop=FALSE) + labs(fill="Variant type",x="Variant size",y="Count",title="All variants > 5 bp") + theme(strip.text=element_blank(),strip.background=element_blank(),plot.title = element_text(vjust=3))
 dev.off()
 
  
