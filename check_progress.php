5b446fbb4bb124873cdcc6a00a51cbc751cb7c0d
MariaNattestad <lunexa@gmail.com>
2016-04-01 14:33:22 -0400

    Consolidated app-specific css into custom_styles.css, removing theme.css

--- check_progress.php
+++ check_progress.php
@@ -4,18 +4,6 @@
     $filename="user_data/" . $code . "/progress.log";
     
     if (file_exists($filename)) {
-        // $myfile = fopen($filename, "r") or die("error");
-        
-        // $progress_stats = "";
-        
-        // while(!feof($myfile)) {
-        //     $line=fgets($myfile);
-        //     // $line=trim(preg_replace( '/\s+/', ' ', $line ));
-        //     if (strlen($line)>0) {
-        //         $progress_stats.append($line);
-        //     }
-        // }
-        // fclose($myfile);
         $progress_stats = file($filename);
         echo json_encode($progress_stats);
     }
