8c3a814eb62235212421f8a602ff978a8d91d4bf
MariaNattestad <lunexa@gmail.com>
2016-04-05 18:35:14 -0400

    Make Nchart publication quality

--- bin/assembly_Nchart.R
+++ bin/assembly_Nchart.R
@@ -1,18 +1,19 @@
+# Author: Maria Nattestad
+# Email: mnattest@cshl.edu
+# This script is part of Assemblytics, a program to detect and analyze structural variants from an assembly aligned to a reference genome using MUMmer. 
+
+
 library(ggplot2)
 library(scales)
 
+
 args<-commandArgs(TRUE)
 prefix <- args[1]
-
-# TESTING:
-# prefix <- "/Applications/XAMPP/htdocs/Assemblytics/user_data/example3/Escherichia_coli_MHAP_assembly"
 
 
 filename_ref <- paste(prefix, ".coords.ref.genome", sep="")
 filename_query <- paste(prefix, ".coords.query.genome", sep="")
 
-
-plot.output.filename <- paste(prefix,".Assemblytics.Nchart.png",sep="")
 
 
 ref.data <- read.csv(filename_ref, sep="\t", quote='',header=FALSE)
@@ -38,20 +39,48 @@ both.plot <- rbind(ref.cumsum,query.cumsum)
 
 
 
-  
-png(file=plot.output.filename,width=1000,height=1000)
 
-theme_set(theme_gray(base_size = 24))
-
-if (nrow(both.plot) > 2) {
-    ggplot(both.plot, aes(x = NG, y = contig.length,color=contig.source)) + xlim(0,100) + scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x), labels = trans_format("log10", math_format(10^.x))) + geom_path(size=2) + labs(x = paste("NG(x)% where 100% = ",genome.length," bp", sep=""),y="Sequence length",colour="Assembly") + annotation_logticks(sides="lr")
-    
-} else {
-    ggplot(both.plot, aes(x = NG, y = contig.length,color=contig.source)) + xlim(0,100) +  geom_point(size=5) + ylim(0,genome.length) + labs(x = paste("NG(x)% where 100% = ",genome.length," bp", sep=""),y="Sequence length",colour="Assembly")
+bp_format<-function(num) {
+    if (num > 1000000000) {
+        paste(formatC(num/1000000000,format="f",digits=3,big.mark=",",drop0trailing = TRUE)," Gbp",sep="")
+    }
+    else if (num > 1000000) {
+        paste(formatC(num/1000000,format="f",digits=3,big.mark=",",drop0trailing = TRUE)," Mbp",sep="")
+    }
+    else {
+        paste(formatC(num,format="f",big.mark=",",drop0trailing = TRUE), " bp", sep="")
+    } 
 }
 
-dev.off()
+theme_set(theme_bw(base_size = 12) + theme(panel.grid.minor = element_line(colour = NA)))
+colors <- c("blue","limegreen")
 
+
+for (to_png in c(TRUE,FALSE)) {
+    
+    if (to_png) {
+        png(file=paste(prefix,".Assemblytics.Nchart.png",sep=""),width=1000,height=1000,res=200)
+    } else {
+        pdf(paste(prefix,".Assemblytics.Nchart.pdf",sep=""))
+    }
+    
+    if (nrow(both.plot) > 2) {
+        print(
+            ggplot(both.plot, aes(x = NG, y = contig.length, color=contig.source)) + 
+                xlim(0,100) + 
+                scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x), labels = trans_format("log10", math_format(10^.x))) + 
+                geom_path(size=2,alpha=0.5) +
+                labs(x = paste("NG(x)% where 100% = ",bp_format(genome.length), sep=""),y="Sequence length",colour="Assembly",title="Cumulative sequence length") +
+                scale_color_manual(values=colors) +
+                annotation_logticks(sides="lr")
+              )
+    } else {
+        # To make bacterial genomes at least show a dot instead of an error because  
+        # they only have 1 contig
+        print(ggplot(both.plot, aes(x = NG, y = contig.length,color=contig.source)) + xlim(0,100) +  geom_point(size=5) + ylim(0,genome.length) + labs(x = paste("NG(x)% where 100% = ",genome.length," bp", sep=""),y="Sequence length",colour="Assembly"))
+    }
+    dev.off()
+}
 
 
 
